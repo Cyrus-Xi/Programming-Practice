1fd6e91b258f59b0158addfb1912c433084ff98a
Cyrus Xi <xi.cyrus@gmail.com>
2017-02-18 13:33:04 -0500

    Add Interview Cake problems 6 and 7

---
+++ InterviewCake/6_rect_love.py
@@ -0,0 +1,34 @@
+def rect_intersect(r1, r2):
+    lx_1, by_1, w_1, h_1 = r1['left_x'], r1['bottom_y'], r1['width'], r1['height']
+    lx_2, by_2, w_2, h_2 = r2['left_x'], r2['bottom_y'], r2['width'], r2['height']
+    
+    # No possible intersection.
+    if lx_1 > lx_2 + w_2 or lx_2 > lx_1 + w_1 or by_1 > by_2 + h_2 or by_2 > by_1 + h_1:
+        return None
+    
+    r3 = {}
+    
+    r3['left_x'], r3['bottom_y']  = max(lx_1, lx_2), max(by_1, by_2)
+    
+    rx_1, rx_2 = lx_1 + w_1, lx_2 + w_2
+    ty_1, ty_2 = by_1 + h_1, by_2 + h_2
+    
+    # Just "touching" counts as an (0 width/height) intersection.
+    rx_3, ty_3 = min(rx_1, rx_2), min(ty_1, ty_2)
+    
+    r3['width']  = rx_3 - r3['left_x']
+    r3['height'] = ty_3 - r3['bottom_y']
+    
+    return r3
+    
+r1 = { 
+    'left_x'  : 1, 'bottom_y' : 2,
+    'width': 5, 'height': 5
+}
+
+r2 = {
+    'left_x'  : 6, 'bottom_y'  : 3,
+    'width': 1, 'height': 2
+}
+
+print rect_intersect(r1, r2)
---
+++ InterviewCake/7_temp_tracker.py
@@ -0,0 +1,83 @@
+class TempTracker:
+    """Optimize for time and space complexity. 
+    
+    All methods are O(1) running time and O(1) space (assuming realistically bounded input).
+    """
+    
+    def __init__(self, list_temps=None):
+        """Can't use an optional mutable argument because would share state."""
+        # Don't actually need to store an instance list of temps.
+        list_t = list_temps if list_temps else []
+        
+        if list_t: self.max_temp, self.min_temp = max(list_t), min(list_t)
+        else: self.max_temp, self.min_temp = None, None
+            
+        self.num_temps = len(list_t)
+        if not list_t: self.avg_temp = None
+        else: self.avg_temp = float(sum(list_t)) / self.num_temps
+        
+        # Technically, since our input list is bounded to temps[0-110] we could 
+        # just use an array. But this is more universal.
+        self.freqs_t = {}
+        self.mode = self.max_freq = 0
+        for k in list_t:
+            val = self.freqs_t.get(k, 0) + 1
+            # Update mode.
+            if val > self.max_freq:
+                # Don't need to update mode if same one.
+                if k != self.mode: self.mode = k
+                self.max_freq = val
+            self.freqs_t[k] = val
+            
+    def insert(self, temp):
+        self.num_temps += 1
+        
+        if not self.num_temps:
+            self.max_temp = temp
+            self.min_temp = temp
+        else:
+            self.max_temp = max(temp, self.max_temp)
+            self.min_temp = min(temp, self.min_temp)
+        
+        # New mean is the old mean plus the (difference between the new value 
+        # and the previous mean) divided by the (new number of values). 
+        # Have to "spread out" the new value's contribution.
+        if not self.avg_temp: self.avg_temp = float(temp)
+        else: self.avg_temp += (temp - self.avg_temp) / self.num_temps
+        
+        # Update mode if new frequency of temp is greater.
+        curr_freq = self.freqs_t.get(temp, 0) + 1
+        if curr_freq > self.max_freq:
+            if temp != self.mode: self.mode = temp
+            self.max_freq = curr_freq
+        self.freqs_t[temp] = curr_freq
+        
+    def get_max(self):
+        return self.max_temp
+    
+    def get_min(self):
+        return self.min_temp
+    
+    def get_mean(self):
+        """Mean is iteratively updated on each insert."""
+        return round(self.avg_temp, 1) if self.avg_temp else self.avg_temp
+        
+    def get_mode(self):
+        return self.mode if self.num_temps else None
+        
+
+test1 = TempTracker()
+test1.insert(1)
+test1.insert(6)
+test1.insert(1)
+print test1.get_max(), test1.get_min(), test1.get_mean(), test1.get_mode()
+
+test2 = TempTracker()
+print test2.get_max(), test2.get_min(), test2.get_mean(), test2.get_mode()
+
+test3 = TempTracker(list_temps=[1, 1, 6])
+print test3.get_max(), test3.get_min(), test3.get_mean(), test3.get_mode()
+test3.insert(21)
+test3.insert(6)
+test3.insert(6)
+print test3.get_max(), test3.get_min(), test3.get_mean(), test3.get_mode()
