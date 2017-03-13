53ddcc70052b002687c8444fa8173424e664c36b
Cyrus Xi <xi.cyrus@gmail.com>
2017-03-12 21:48:12 -0400

    Add second implementation of IC #31 with timing code

--- InterviewCake/31_recursive_str_permutations.py
+++ InterviewCake/31_recursive_str_permutations.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-def gen_permutations(s):
+def gen_permutes(s):
     if len(s) < 2: return set([s])
-    new_perms = gen_permutations(s[1:])
+    new_perms = gen_permutes(s[1:])
     perms = set()
     for p in new_perms:
         # Add the first character at each position in the permutation.
@@ -11,4 +11,36 @@ def gen_permutations(s):
             perms.add(new_p)
     return perms
                          
-print gen_permutations('abc')
+def gen_permutes2(s, st, end):
+    if st == end: return set([s])
+    perms, s = set(), list(s)
+    for i in xrange(st, end + 1):
+        s[st], s[i] = s[i], s[st]
+        perms.update(gen_permutes2(''.join(s), st + 1, end))
+        # Backtrack.
+        s[i], s[st] = s[st], s[i]
+    return perms
+
+
+if __name__ == '__main__':
+    from timeit import timeit
+
+    def wrapper(func, *args, **kwargs):
+        def wrapped():
+            return func(*args, **kwargs)
+        return wrapped
+
+    s = 'aabfcd'
+    wrapped = wrapper(gen_permutes, s)
+    wrapped2 = wrapper(gen_permutes2, s, 0, 5)
+
+    g1 = round(timeit(wrapped, number=10000), 3)
+    print 'gen_permutes of {} took {} seconds to get:'.format(s, str(g1))
+    print gen_permutes('aabcd')
+
+    print
+    g2 = round(timeit(wrapped2, number=10000) * 1000, 3)
+    print 'gen_permutes2 of {} took {} seconds to get:'.format(s, str(g1))
+    print gen_permutes2('aabcd', 0, 4)
+
+
