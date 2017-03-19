c0fcadb78ebf881e199003c37eb4f95d4b4cfbcd
Cyrus Xi <xi.cyrus@gmail.com>
2017-03-18 21:00:50 -0400

    Add description for LC #186 reverse words in a string II

--- Leetcode/186_reverse_words_in_a_string_II.py
+++ Leetcode/186_reverse_words_in_a_string_II.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+"""
+Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
+
+The input string does not contain leading or trailing spaces and the words are always separated by a single space.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+Could you do it in-place without allocating extra space?
+"""
+
 def reverse(arr, l, r):
     while l < r:
         arr[l], arr[r] = arr[r], arr[l]
